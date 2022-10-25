a909322713d4eccc4f7e2497ba3ec01d861810cb
ValeryLapo <lappo.valera@yandex.ru>
2022-10-25 16:35:50 +0300

    Lesson 2 done

---
+++ Lesson 2/Structure.Middleware/01.Structure/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _01.Structure
+{
+    public class Program
+    {
+        // точка входа в приложение. Настривает наш Host
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
