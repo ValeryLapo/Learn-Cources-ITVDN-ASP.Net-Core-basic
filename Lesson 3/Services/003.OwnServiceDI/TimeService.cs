8f82c782212333232723dc1010d3d4511089d6e4
ValeryLapo <lappo.valera@yandex.ru>
2022-10-26 16:22:43 +0300

    Lesson 3 done

---
+++ Lesson 3/Services/003.OwnServiceDI/TimeService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _003.OwnServiceDI
+{
+    public interface ITimeService
+    {
+        string GetTime();
+    }
+
+
+    public class TimeServiceFull : ITimeService
+    {
+        public string GetTime()
+        {
+            return DateTime.Now.ToString("hh:mm:ss");
+        }
+    }
+
+    public class TimeServiceHoursMinutes : ITimeService
+    {
+        public string GetTime()
+        {
+            return DateTime.Now.ToString("hh:mm");
+        }
+    }
+
+    public class TimeServiceHours : ITimeService
+    {
+        public string GetTime()
+        {
+            return DateTime.Now.ToString("hh");
+        }
+    }
+}
---
+++ Lesson 3/Services/OwnService/TimeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OwnService
+{
+    public class TimeService
+    {
+        public string GetTime()
+        {
+            return DateTime.Now.ToString("hh:mm:ss");
+        }
+    }
+}
